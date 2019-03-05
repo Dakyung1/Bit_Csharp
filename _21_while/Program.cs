b857b568eba941305fa36f70d6c20f26cec63556
Dakyung1 <38394152+Dakyung1@users.noreply.github.com>
2019-03-05 11:27:00 +0900

    first

---
+++ _21_while/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+/*문제
+ while문을 이용해서 1부터 100까지 합을 출력하세요.
+ */
+namespace _21_while
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int num = 0;
+            int result = 0;
+            while (num < 100)
+            {
+                ++num;
+                result += num;
+            }
+            Console.WriteLine("합은" + result);
+        }
+    }
+}
